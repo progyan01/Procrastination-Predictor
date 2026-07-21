834906406948cb5bd9aa12a9fc3c36f510e1182d
pragyan06 <pragyan0888@gmail.com>
2026-07-21 22:04:54 +0530

    Replaced LR with random forest model

--- src/model/train.py
+++ src/model/train.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
@@ -34,7 +34,7 @@ def train(X, y):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=TEST_SIZE, random_state=42, stratify=y
     )
-    model = LogisticRegression(class_weight="balanced", max_iter=1000)
+    model = RandomForestClassifier(n_estimators=100, class_weight="balanced", random_state=42)
     model.fit(X_train, y_train)
     print(classification_report(y_test, model.predict(X_test)))
     return model
